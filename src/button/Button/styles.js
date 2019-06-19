12af4b5f3822323c6b7655917330c0ae141d8cec
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-06-19 14:32:19 +0200

    :lipstick: Update basic styles

--- src/button/Button/styles.js
+++ src/button/Button/styles.js
@@ -1,10 +1,12 @@
-export const styles = {
+export const styles = ({ isFilled = false }) => ({
   padding: '15px 20px',
-  borderWidth: 0,
-  textDecoration: 'none',
-  backgroundColor: '#24D58D',
-  color: 'white',
+  borderWidth: '1.4px',
+  borderColor: '#24D58D',
   borderRadius: '100px',
+  textDecoration: 'none',
+  color: isFilled ? 'white' : '#24D58D',
+  backgroundColor: isFilled ? '#24D58D' : 'transparent',
+  fontSize: '15px',
   transition: 'box-shadow .3s ease-in-out',
   '&:hover': {
     boxShadow: '0 1px 4px 1px rgba(0,0,0,0.3)',
@@ -13,4 +15,4 @@ export const styles = {
     opacity: '.5',
     pointerEvents: 'none',
   },
-}
+})
