2deaaa39318d9e8bed9dd9e331ccc65889eceead
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-06-19 16:07:47 +0200

    :pencil: Use knobs in button storybooks

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,16 +3,16 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withA11y } from '@storybook/addon-a11y'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, boolean, text } from '@storybook/addon-knobs'
 
 import { Button } from '../src/index'
 
 storiesOf('Components|Button/Button', module)
   .addDecorator(withA11y)
   .addDecorator(withKnobs)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => <Button onClick={action('clicked')} disabled={boolean('Disabled', false)}>{text('Label', 'Validate')}</Button>)
   .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+    <Button onClick={action('clicked')} disabled={boolean('Disabled', false)}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
--- stories/icon-button.stories.js
+++ stories/icon-button.stories.js
@@ -3,13 +3,13 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withA11y } from '@storybook/addon-a11y'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
 import { IconButton } from '../src/index'
 
 storiesOf('Components|Button/IconButton', module)
   .addDecorator(withA11y)
   .addDecorator(withKnobs)
-  .add('with icon name', () => <IconButton icon="times" onClick={action('clicked')}></IconButton>)
-  .add('with prefix', () => <IconButton prefix="fab" icon="instagram" onClick={action('clicked')}></IconButton>)
+  .add('with icon name', () => <IconButton icon={text('fa icon', 'times')} onClick={action('clicked')}></IconButton>)
+  .add('with prefix', () => <IconButton prefix={text('fa prefix', 'fab')} icon={text('fa icon', 'instagram')} onClick={action('clicked')}></IconButton>)
   .add('without icon name', () => <IconButton onClick={action('clicked')} />)
