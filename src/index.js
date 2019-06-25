b2a17bb23dac73a728cac8e18257d77a47354809
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-06-25 11:29:58 +0200

    :package: Global entrypoint export everything

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-import { library as iconLibrary } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-iconLibrary.add(fas, fab)
-
-export { Button } from './components/button/Button/Button.jsx'
-export { IconButton } from './components/button/IconButton.jsx'
+export * from './components/index.js'
+export * from './colors/index.js'
