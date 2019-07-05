fab7c09b3cf3357b99d4a7e99456595b37c045b7
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-07-05 17:08:23 +0200

    :lipstick: Update colors

--- src/colors/colors.js
+++ src/colors/colors.js
@@ -1,25 +1,54 @@
 /* Reds */
+export const R500_COLOR = '#B72A2A'
 export const R400_COLOR = '#ED4A4A'
 export const R300_COLOR = '#FF5D5D'
+export const R200_COLOR = '#F8B7B7'
+export const R100_COLOR = '#FBE7E7'
 
-/* Teals */
-export const T500_COLOR = '#008D94'
-export const T300_COLOR = '#00BECD'
+/* Cyans */
+export const C500_COLOR = '#00474A'
+export const C400_COLOR = '#005F67'
+export const C300_COLOR = '#008D94'
+export const C200_COLOR = '#00BECD'
+export const C100_COLOR = '#99D1D4'
+export const C75_COLOR = '#99E5EB'
+export const C50_COLOR = '#D3E9EA'
+export const C25_COLOR = '#E5F1F0'
+export const C15_COLOR = '#EFF9F9'
 
 /* Greens */
-export const G200_COLOR = '#24D58D'
+export const G500_COLOR = '#126b47'
+export const G400_COLOR = '#24D58D'
+export const G300_COLOR = '#A7EED1'
+export const G200_COLOR = '#E9FBF4'
 
 /* Yellows */
-export const Y500_COLOR = '#FC883A'
-export const Y300_COLOR = '#FEC138'
-export const Y100_COLOR = '#FFF173'
+export const Y500_COLOR = '#F7D00F'
+export const Y400_COLOR = '#FFF173'
+export const Y300_COLOR = '#FFF9C7'
+export const Y200_COLOR = '#FFFDF0'
+
+/* Oranges */
+export const O500_COLOR = '#BD550F'
+export const O400_COLOR = '#DB8E31'
+export const O300_COLOR = '#FC883A'
+export const O200_COLOR = '#FEC138'
+export const O100_COLOR = '#FECFB0'
+export const O75_COLOR = '#FFE6AF'
+export const O50_COLOR = '#FEEFE4'
+export const O25_COLOR = '#FFFBF0'
 
 /* Purples */
+export const P500_COLOR = '#552D7C'
 export const P400_COLOR = '#9654CB'
+export const P300_COLOR = '#D5BBEA'
+export const P200_COLOR = '#EFE7F5'
 
 /* Blues */
+export const B500_COLOR = '#145182'
 export const B400_COLOR = '#4A9CED'
-export const B100_COLOR = '#E5F1F0'
+export const B300_COLOR = '#B7D7F8'
+export const B200_COLOR = '#E7F1FB'
 
 /* Neutrals */
 export const N500_COLOR = '#363636'
