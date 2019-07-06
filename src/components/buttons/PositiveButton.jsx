144afb49d8d3a87b9f90b3f37124e926545a02af
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-07-06 08:11:17 +0200

    :bug: Add missing prop types

--- src/components/buttons/PositiveButton.jsx
+++ src/components/buttons/PositiveButton.jsx
@@ -3,3 +3,7 @@ import { Button } from './Button/Button.jsx'
 import { PRIMARY_COLORSET } from '../../colors/index.js'
 
 export const PositiveButton = props => <Button {...props} color={PRIMARY_COLORSET.SHAMROCK} />
+
+PositiveButton.propTypes = {
+  ...Button.propTypes,
+}
