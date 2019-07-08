571dfd40cce1e38c8e9a2278025a135bb0ec18df
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-07-08 13:26:12 +0200

    :construction: Create a function to automatically compute color shades

---
+++ src/utils/colors.js
@@ -0,0 +1,26 @@
+import { min, max } from './number.js'
+
+export function colorShades(color, rates = 0) {
+  if (color == null) return null
+
+  const [hash, ...colorArray] = color
+
+  if (hash !== '#' || colorArray.length !== 6) return null
+
+  const colorNumber = parseInt(colorArray.join(''), 16)
+
+  let redComponent = (colorNumber >> 16) + rates
+  let greenComponent = (colorNumber & 0x0000FF) + rates
+  let blueComponent = ((colorNumber >> 8) & 0x00FF) + rates
+
+  redComponent = min(max(redComponent, 255), 0)
+  greenComponent = min(max(greenComponent, 255), 0)
+  blueComponent = min(max(blueComponent, 255), 0)
+
+  const ajustedColorNumber = greenComponent | (blueComponent << 8) | (redComponent << 16)
+  const ajustedColorString = ajustedColorNumber
+    .toString(16)
+    .padStart(6, 0)
+
+  return `#${ajustedColorString}`
+}
