0aa01ab6e33c25ff5dc86930724bb6e019dc5007
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-07-08 13:25:36 +0200

    :construction: Create method to force min and max values

---
+++ src/utils/number.js
@@ -0,0 +1,15 @@
+export function max(num, max) {
+  if (num > max) {
+    return max
+  }
+
+  return num
+}
+
+export function min(num, min) {
+  if (num < min) {
+    return min
+  }
+
+  return num
+}
