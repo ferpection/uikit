d7d9e0f9467c4016a07f370d85477b79b677e05c
Mindsers <dev@nathanaelcherrier.com>
2019-08-22 00:06:51 +0200

    :alembic::label: Add a handwritten declaration file

---
+++ src/components/index.d.ts
@@ -0,0 +1,309 @@
+declare module '@ferpection/uikit' {
+    import React from 'react'
+
+    export const R500_COLOR: string
+    export const R400_COLOR: string
+    export const R300_COLOR: string
+    export const R200_COLOR: string
+    export const R100_COLOR: string
+    export const C500_COLOR: string
+    export const C400_COLOR: string
+    export const C300_COLOR: string
+    export const C200_COLOR: string
+    export const C100_COLOR: string
+    export const C75_COLOR : string
+    export const C50_COLOR : string
+    export const C25_COLOR : string
+    export const C15_COLOR : string
+    export const G500_COLOR: string
+    export const G400_COLOR: string
+    export const G300_COLOR: string
+    export const G200_COLOR: string
+    export const Y500_COLOR: string
+    export const Y400_COLOR: string
+    export const Y300_COLOR: string
+    export const Y200_COLOR: string
+    export const O500_COLOR: string
+    export const O400_COLOR: string
+    export const O300_COLOR: string
+    export const O200_COLOR: string
+    export const O100_COLOR: string
+    export const O75_COLOR: string
+    export const O50_COLOR: string
+    export const O25_COLOR: string
+    export const P500_COLOR: string
+    export const P400_COLOR: string
+    export const P300_COLOR: string
+    export const P200_COLOR: string
+    export const B500_COLOR: string
+    export const B400_COLOR: string
+    export const B300_COLOR: string
+    export const B200_COLOR: string
+    export const N500_COLOR: string
+    export const N400_COLOR: string
+    export const N300_COLOR: string
+    export const N200_COLOR: string
+    export const N100_COLOR: string
+    export const N75_COLOR : string
+
+    export const PRIMARY_COLORSET: {
+        CINNABAR: string,
+        ROBINS_EGG_BLUE: string,
+        SHAMROCK: string,
+        SUNGLOW: string
+    }
+
+    export const SECONDARY_COLORSET: {
+        AMETHYST: string,
+        PICTON_BLUE: string,
+        CRUSTA: string,
+        BLUE_LAGOON: string,
+        AQUA_HAZE: string,
+        PARIS_DAISY: string,
+    }
+
+    export const FULL_COLORSET: {
+        CINNABAR: string,
+        ROBINS_EGG_BLUE: string,
+        SHAMROCK: string,
+        SUNGLOW: string,
+        AMETHYST: string,
+        PICTON_BLUE: string,
+        CRUSTA: string,
+        BLUE_LAGOON: string,
+        AQUA_HAZE: string,
+        PARIS_DAISY: string,
+        BITTERSWEET: string,
+        MINE_SHAFT: string,
+        EMPEROR: string,
+        BOULDER: string,
+        SILVER_CHALICE: string,
+        ALTO: string,
+        SEASHELL: string,
+        TALL_POPPY: string,
+        ILLUSION: string,
+        LINEN: string,
+        SHERPA_BLUE: string,
+        BLUE_STONE: string,
+        SINBAD: string,
+        BLIZZARD_BLUE: string,
+        BOTTICELLI: string,
+        BLACK_SQUEEZE: string,
+        CHATHAMS_BLUE: string,
+        SAIL: string,
+        SELAGO: string,
+        JEWEL: string,
+        MAGIC_MINT: string,
+        WHITE_ICE: string,
+        RIPE_LEMON: string,
+        LEMON_CHIFFON: string,
+        ISLAND_SPICE: string,
+        EMINENCE: string,
+        FRENCH_LILAC: string,
+        SNUFF: string,
+        TIA_MARIA: string,
+        BRANDY_PUNCH: string,
+        LIGHT_APRICOT: string,
+        PEACH: string,
+        BRIDESMAID: string,
+        FLORAL_WHITE: string,
+    }
+
+    interface ButtonProps {
+        isFilled?: boolean
+        isRaw?: boolean
+        isDisabled?: boolean
+        color?: string
+        hoverColor?: string
+        icon?: string
+        iconStore?: 'fas'|'fab'
+        ariaLabel?: string
+        actionType?: 'default'|'positive'|'negative'
+        to?: string
+    }
+
+    class Button extends React.Component<ButtonProps> {}
+
+    interface HeaderLinkProps extends ButtonProps {
+        headerLigthVersion?: boolean
+    }
+
+    class HeaderLink extends React.Component<HeaderLinkProps> {}
+
+    const ThemeContext: React.Context<{
+        colors: {
+            action: string
+            positive: string
+            negative: string
+        }
+    }>
+
+    interface HeaderProps {
+        homeUrl?: string
+        isLightVersion?: boolean
+    }
+
+    class Header extends React.Component<HeaderProps> {}
+
+    class FerpectionLogo extends React.Component<{
+        color: string
+    }> {}
+
+    export { Button, HeaderLink, ThemeContext, Header, FerpectionLogo }
+}
+
+declare module '@ferpection/uikit/dist/components' {
+    import React from 'react'
+
+    interface ButtonProps {
+        isFilled?: boolean
+        isRaw?: boolean
+        isDisabled?: boolean
+        color?: string
+        hoverColor?: string
+        icon?: string
+        iconStore?: 'fas'|'fab'
+        ariaLabel?: string
+        actionType?: 'default'|'positive'|'negative'
+        to?: string
+    }
+
+    class Button extends React.Component<ButtonProps> {}
+
+    interface HeaderLinkProps extends ButtonProps {
+        headerLigthVersion?: boolean
+    }
+
+    class HeaderLink extends React.Component<HeaderLinkProps> {}
+
+    const ThemeContext: React.Context<{
+        colors: {
+            action: string
+            positive: string
+            negative: string
+        }
+    }>
+
+    interface HeaderProps {
+        homeUrl?: string
+        isLightVersion?: boolean
+    }
+
+    class Header extends React.Component<HeaderProps> {}
+
+    class FerpectionLogo extends React.Component<{
+        color: string
+    }> {}
+
+    export { Button, HeaderLink, ThemeContext, Header, FerpectionLogo }
+}
+
+declare module "@ferpection/uikit/dist/colors" {
+    export const R500_COLOR: string
+    export const R400_COLOR: string
+    export const R300_COLOR: string
+    export const R200_COLOR: string
+    export const R100_COLOR: string
+    export const C500_COLOR: string
+    export const C400_COLOR: string
+    export const C300_COLOR: string
+    export const C200_COLOR: string
+    export const C100_COLOR: string
+    export const C75_COLOR : string
+    export const C50_COLOR : string
+    export const C25_COLOR : string
+    export const C15_COLOR : string
+    export const G500_COLOR: string
+    export const G400_COLOR: string
+    export const G300_COLOR: string
+    export const G200_COLOR: string
+    export const Y500_COLOR: string
+    export const Y400_COLOR: string
+    export const Y300_COLOR: string
+    export const Y200_COLOR: string
+    export const O500_COLOR: string
+    export const O400_COLOR: string
+    export const O300_COLOR: string
+    export const O200_COLOR: string
+    export const O100_COLOR: string
+    export const O75_COLOR: string
+    export const O50_COLOR: string
+    export const O25_COLOR: string
+    export const P500_COLOR: string
+    export const P400_COLOR: string
+    export const P300_COLOR: string
+    export const P200_COLOR: string
+    export const B500_COLOR: string
+    export const B400_COLOR: string
+    export const B300_COLOR: string
+    export const B200_COLOR: string
+    export const N500_COLOR: string
+    export const N400_COLOR: string
+    export const N300_COLOR: string
+    export const N200_COLOR: string
+    export const N100_COLOR: string
+    export const N75_COLOR : string
+
+    export const PRIMARY_COLORSET: {
+        CINNABAR: string,
+        ROBINS_EGG_BLUE: string,
+        SHAMROCK: string,
+        SUNGLOW: string
+    }
+
+    export const SECONDARY_COLORSET: {
+        AMETHYST: string,
+        PICTON_BLUE: string,
+        CRUSTA: string,
+        BLUE_LAGOON: string,
+        AQUA_HAZE: string,
+        PARIS_DAISY: string,
+    }
+
+    export const FULL_COLORSET: {
+        CINNABAR: string,
+        ROBINS_EGG_BLUE: string,
+        SHAMROCK: string,
+        SUNGLOW: string,
+        AMETHYST: string,
+        PICTON_BLUE: string,
+        CRUSTA: string,
+        BLUE_LAGOON: string,
+        AQUA_HAZE: string,
+        PARIS_DAISY: string,
+        BITTERSWEET: string,
+        MINE_SHAFT: string,
+        EMPEROR: string,
+        BOULDER: string,
+        SILVER_CHALICE: string,
+        ALTO: string,
+        SEASHELL: string,
+        TALL_POPPY: string,
+        ILLUSION: string,
+        LINEN: string,
+        SHERPA_BLUE: string,
+        BLUE_STONE: string,
+        SINBAD: string,
+        BLIZZARD_BLUE: string,
+        BOTTICELLI: string,
+        BLACK_SQUEEZE: string,
+        CHATHAMS_BLUE: string,
+        SAIL: string,
+        SELAGO: string,
+        JEWEL: string,
+        MAGIC_MINT: string,
+        WHITE_ICE: string,
+        RIPE_LEMON: string,
+        LEMON_CHIFFON: string,
+        ISLAND_SPICE: string,
+        EMINENCE: string,
+        FRENCH_LILAC: string,
+        SNUFF: string,
+        TIA_MARIA: string,
+        BRANDY_PUNCH: string,
+        LIGHT_APRICOT: string,
+        PEACH: string,
+        BRIDESMAID: string,
+        FLORAL_WHITE: string,
+    }
+}
