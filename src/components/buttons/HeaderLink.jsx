28dc158540c0b17a52e90114b4c6c91afcda23ab
Mindsers <dev@nathanaelcherrier.com>
2019-08-21 11:35:44 +0200

    :bug: Choose what attribute to pass down to other components

--- src/components/buttons/HeaderLink.jsx
+++ src/components/buttons/HeaderLink.jsx
@@ -5,15 +5,19 @@ import { PRIMARY_COLORSET } from '../../colors/index.js'
 
 import { Button } from './Button/Button.jsx'
 
-export const HeaderLink = props => (
-  <Button
-    {...props}
-    color={props.headerLigthVersion ? PRIMARY_COLORSET.CINNABAR : '#FFFFFF'}
-    isRaw
-  >
-    {props.children}
-  </Button>
-)
+export const HeaderLink = props => {
+  const { headerLigthVersion, children, ...otherProps } = props
+
+  return (
+    <Button
+      {...otherProps}
+      color={headerLigthVersion ? PRIMARY_COLORSET.CINNABAR : '#FFFFFF'}
+      isRaw
+    >
+      {children}
+    </Button>
+  )
+}
 
 HeaderLink.propTypes = {
   ...Button.propTypes,
