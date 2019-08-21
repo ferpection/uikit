63f7fcc03aabfcd48d5fbcfdb047f4f6a8b30ae9
Mindsers <dev@nathanaelcherrier.com>
2019-08-22 00:22:11 +0200

    :sparkles: Expose a declaration file

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,32 @@
+import fs from 'fs'
+import path from 'path'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
+const copy = function(options) {
+  return {
+    generateBundle() {
+      const targDir = path.dirname(options.target)
+      if (!fs.existsSync(targDir)) {
+        fs.mkdirSync(targDir)
+      }
+      fs.writeFileSync(options.target, fs.readFileSync(options.src))
+    },
+  }
+}
+
 const globalConf = {
-  external: Object.keys(pkg.dependencies),
+  external: [
+    ...Object.keys(pkg.dependencies),
+    '@emotion/css',
+  ],
   plugins: [
     babel({
       exclude: ['node_modules/**'],
+    }),
+    copy({
+      src: './src/index.d.ts',
+      target: './dist/index.d.ts',
     }),
   ],
 }
