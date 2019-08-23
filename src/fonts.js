85a11baea8828c77a02fd28a597e3d5313be26d5
Mindsers <dev@nathanaelcherrier.com>
2019-08-23 11:11:53 +0200

    :sparkles: Add fonts as constant

---
+++ src/fonts.js
@@ -0,0 +1,19 @@
+export const OPENSANS_FONT = 'Open Sans'
+export const HELVETICANEUE_LIGHT_FONT = 'HelveticaNeue-Light'
+export const HELVETICANEUE_LIGHT_MAC_FONT = 'Helvetica Neue Light'
+export const HELVETICANEUE_FONT = 'Helvetica Neue'
+export const HELVETICA_FONT = 'Helvetica'
+export const ARIAL_FONT = 'Arial'
+export const LUCINDAGRANDE_FONT = 'Lucida Grande'
+export const SANSSERIF_FONT = 'sans-serif'
+
+export const SANSSERIF_FONTSET = Object.freeze([
+  OPENSANS_FONT,
+  HELVETICANEUE_LIGHT_MAC_FONT,
+  HELVETICANEUE_LIGHT_FONT,
+  HELVETICANEUE_FONT,
+  HELVETICA_FONT,
+  ARIAL_FONT,
+  LUCINDAGRANDE_FONT,
+  SANSSERIF_FONT,
+])
