88e2ddef9e2a2ce8c2ada5959e53c07123509aab
Mindsers <dev@nathanaelcherrier.com>
2019-08-26 11:45:46 +0200

    :pencil: Add stories for FormError

---
+++ stories/form-errors.stories.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+import { withKnobs, object } from '@storybook/addon-knobs'
+
+import { FormErrors } from '../src/components/index.js'
+
+storiesOf('Components|Forms/FormErrors', module)
+  .addDecorator(withA11y)
+  .addDecorator(withKnobs)
+  .add('single error', () => (
+    <>
+      <FormErrors
+        errors={{ required: {} }}
+      />
+    </>
+  ))
+  .add('multiple errors', () => (
+    <>
+      <FormErrors
+        errors={object('errors', { required: {}, notAnEmail: { value: 'this is not an email' } })}
+      />
+    </>
+  ))
