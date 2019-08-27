f48818549a876d048359e4640cbec7b3207c214c
Mindsers <dev@nathanaelcherrier.com>
2019-08-27 17:37:42 +0200

    :constrcution: Create component for the ordered fields

---
+++ stories/ordered-field-list.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+
+import { OrderedFieldList } from '../src/components/index.js'
+
+storiesOf('Components|Forms/OrderedFieldList', module)
+  .addDecorator(withA11y)
+  .add('single error', () => <OrderedFieldList />)
