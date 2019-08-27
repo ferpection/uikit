75707ba336e77780bbdf38c2f0e8e67ba8df1937
Mindsers <dev@nathanaelcherrier.com>
2019-08-27 22:02:42 +0200

    :sparkles: Dynamically add fields

--- src/components/forms/OrderedFieldList/OrderedFieldList.jsx
+++ src/components/forms/OrderedFieldList/OrderedFieldList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import propTypes from 'prop-types'
 
 import { TextField } from '../TextField/TextField.jsx'
@@ -7,19 +7,24 @@ import { Button } from '../../buttons/Button/Button.jsx'
 import { listStyles, trashStyles, listItemStyles } from './styles'
 
 export const OrderedFieldList = (props) => {
+  const [values, setValues] = useState([''])
+
   const { isDisabled } = props
-  const fields = ['réponse un', 'réponse deux']
+
+  const handleAddition = () => setValues([...values, ''])
+  const handleDeletion = () => {}
 
   return (
     <ol css={[listStyles]}>
-      {fields.map(field => (
-        <li css={[listItemStyles]}>
+      {values.map((value, i) => (
+        <li key={i} css={[listItemStyles]}>
           <span css={[trashStyles]}>
-            <Button isRaw icon="trash" />
+            <Button isRaw icon="trash" onClick={handleDeletion} />
           </span>
-          <TextField isDisabled={isDisabled} value={field} />
+          <TextField isDisabled={isDisabled} value={value} />
         </li>
       ))}
+      <Button icon="plus" onClick={handleAddition}>Ajouter</Button>
     </ol>
   )
 }
