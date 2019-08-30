e82267e6de72255513f67d17ba515389a4344110
Mindsers <dev@nathanaelcherrier.com>
2019-08-30 17:07:40 +0200

    :sparkles: Create a component to display ferpection badge icons

---
+++ stories/ferpection-badge-icon.stories.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
+import { FerpectionBadgeIcon } from '../src/components/index.js'
+import { FULL_COLORSET } from '../src/colors/index.js'
+
+storiesOf('Iconography|FerpectionBadgeIcon', module)
+  .addDecorator(withA11y)
+  .addDecorator(withKnobs)
+  .add('normal state', () => (
+    <FerpectionBadgeIcon
+      icon={select(
+        'icon',
+        [
+          'config',
+          'goals',
+          'incentive',
+          'objectives',
+          'planning',
+          'post-test',
+          'pre-test',
+          'screening',
+          'target',
+          'test',
+          'tasks',
+        ],
+        'config'
+      )}
+    />
+  ))
+  .add('w/ forced border color', () => (
+    <FerpectionBadgeIcon
+      icon={select(
+        'icon',
+        [
+          'config',
+          'goals',
+          'incentive',
+          'objectives',
+          'planning',
+          'post-test',
+          'pre-test',
+          'screening',
+          'target',
+          'test',
+          'tasks',
+        ],
+        'config'
+      )}
+      borderColor={FULL_COLORSET.CINNABAR}
+    />
+  ))
--- stories/thematic-section.stories.js
+++ stories/thematic-section.stories.js
@@ -1,31 +1,49 @@
 import React from 'react'
-import { css } from '@emotion/core';
+import { css } from '@emotion/core'
 
 import { storiesOf } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs } from '@storybook/addon-knobs'
 
-import { ThematicSection, Button, TextFieldList, HelpBlock } from '../src/components/index.js'
+import {
+  ThematicSection,
+  Button,
+  TextFieldList,
+  HelpBlock,
+} from '../src/components/index.js'
 
 storiesOf('Layout|ThematicSection', module)
   .addDecorator(withA11y)
   .addDecorator(withKnobs)
   .add('with actions buttons', () => (
-    <ThematicSection title="Questions de qualification" actions={<Button icon="comment-medical" isDisabled isRaw />} />
+    <ThematicSection
+      title="Questions de qualification"
+      actions={<Button icon="comment-medical" isDisabled isRaw />}
+    />
   ))
   .add('with form components', () => (
     <ThematicSection title="Questions de qualification">
-      <div css={css`display: flex;`}>
-        <div css={css`width: 50%;`}>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <div
+          css={css`
+            width: 50%;
+          `}
+        >
           <p>
-            Consequat culpa qui enim laboris voluptate proident tempor cillum laboris est ex sint id. Anim tempor exercitation amet consequat et excepteur ex. In anim occaecat adipisicing velit Lorem. Est sunt excepteur ut laborum commodo qui sit veniam tempor nulla cillum adipisicing. Do ipsum culpa excepteur mollit.
+            Consequat culpa qui enim laboris voluptate proident tempor cillum
+            laboris est ex sint id. Anim tempor exercitation amet consequat et
+            excepteur ex. In anim occaecat adipisicing velit Lorem. Est sunt
+            excepteur ut laborum commodo qui sit veniam tempor nulla cillum
+            adipisicing. Do ipsum culpa excepteur mollit.
           </p>
           <TextFieldList placeholder="Choix.." isEditable />
         </div>
-        <HelpBlock question="Pourquoi commencer par les objectifs ?" >
-          <p>
-            Pour conduire une étude efficace, lorem ipsum
-          </p>
+        <HelpBlock question="Pourquoi commencer par les objectifs ?">
+          <p>Pour conduire une étude efficace, lorem ipsum</p>
           <p>Par exemple :</p>
           <ul>
             <li>choix un</li>
