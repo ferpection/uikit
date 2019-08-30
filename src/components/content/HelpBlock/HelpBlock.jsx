d18b54433c8fbe7cb51fb000e4e388998c6e4026
Mindsers <dev@nathanaelcherrier.com>
2019-08-30 13:47:15 +0200

    :lipstick: Add icon on helpblock

--- src/components/content/HelpBlock/HelpBlock.jsx
+++ src/components/content/HelpBlock/HelpBlock.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { containerStyles, questionStyles } from './styles';
+import { containerStyles, questionStyles, logoStyles, headerStyles, questionTextStyles, logoOuterStyles } from './styles'
 
 export const HelpBlock = (props) => {
   return (
     <aside css={[containerStyles]}>
-      <header>
+      <header css={headerStyles}>
+        <div css={logoOuterStyles}>
+          <div css={logoStyles}>
+            <FontAwesomeIcon icon={['far', 'lightbulb']} size="lg" />
+          </div>
+        </div>
         <h2 css={questionStyles}>{props.question}</h2>
       </header>
-      <section>
+      <section css={questionTextStyles}>
         {props.children}
       </section>
     </aside>
@@ -17,5 +23,5 @@ export const HelpBlock = (props) => {
 }
 
 HelpBlock.propTypes = {
-  question: propTypes.string.isRequired
+  question: propTypes.string.isRequired,
 }
--- src/components/content/HelpBlock/styles.js
+++ src/components/content/HelpBlock/styles.js
@@ -4,16 +4,49 @@ import { SANSSERIF_FONTSET } from '../../../fonts'
 import { N300_COLOR, R400_COLOR } from '../../../colors'
 
 export const containerStyles = css`
-  padding: 0 20px;
-  margin: 15px;
+  padding: 0 25px;
+  margin: 20px;
   font-family: ${`${SANSSERIF_FONTSET}`};
-  font-size: 15px;
+  font-size: 13.5px;
   color: ${`${N300_COLOR}`};
   border-left: 1px solid ${`${R400_COLOR}`};
 `
 
 export const questionStyles = css`
-  margin: 0 0 25px;
+  margin: 7px 0 20px;
   font-weight: lighter;
-  font-size: 16px;
+  font-size: 15px;
+`
+
+export const headerStyles = css`
+  display: flex;
+  align-items: center;
+  position: relative;
+`
+
+export const logoOuterStyles = css`
+  padding: 0 0 10px 0;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+  left: -41px;
+  top: 0;
+`
+
+export const logoStyles = css`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 1px solid ${`${R400_COLOR}`};
+  color: ${`${R400_COLOR}`};
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const questionTextStyles = css`
+  p {
+    margin-top: 0;
+  }
 `
