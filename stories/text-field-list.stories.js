a14734cd8349c86ef92a2cc0ae458cf936eff2bf
Mindsers <dev@nathanaelcherrier.com>
2019-10-15 16:33:53 +0200

    :pencil: Add event on storybook to show focus in/out on TextFieldList

--- stories/text-field-list.stories.js
+++ stories/text-field-list.stories.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
+import { action } from '@storybook/addon-actions'
 import {
   withKnobs,
   boolean,
@@ -57,6 +58,8 @@ storiesOf('Components|Forms/TextFieldList', module)
             buttonText={text('button text', 'Add a list item')}
             isEditable
             onValueChange={values => setValues(values)}
+            onFocus={action('focus in')}
+            onBlur={action('focus out')}
           />
           <pre
             css={{
