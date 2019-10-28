bde821e2d4d0d662de43e9a3b87e8d49a6364f0e
Nathanael Cherrier <dev@nathanaelcherrier.com>
2019-10-28 12:11:11 +0100

    :pencil: Add documentation on disabled mode

--- stories/radio-field.stories.js
+++ stories/radio-field.stories.js
@@ -20,9 +20,35 @@ storiesOf('Components|Forms/RadioField', module)
   ))
   .add('disabled state', () => (
     <>
+      Disabled state must be added on the <code>{RadioField.name}</code> directly. Not on the <code>{RadioButton.name}</code>.
+      <h2><code>isDisabled</code> on <code>{RadioField.name}</code></h2>
+      <pre>
+        <code>
+          &lt;RadioField isDisabled&gt;<br/>
+            &lt;RadioButton&gt;Choice One&lt;/RadioButton&gt;<br/>
+            &lt;RadioButton&gt;Choice Two&lt;/RadioButton&gt;<br/>
+            &lt;RadioButton&gt;Choice Three&lt;/RadioButton&gt;<br/>
+          &lt;/RadioField&gt;<br/>
+        </code>
+      </pre>
       <RadioField isDisabled>
         <RadioButton>Choice One</RadioButton>
         <RadioButton>Choice Two</RadioButton>
+        <RadioButton>Choice Three</RadioButton>
+      </RadioField>
+      <h2><code>isDisabled</code> on <code>{RadioButton.name}</code></h2>
+      <pre>
+        <code>
+          &lt;RadioField&gt;<br/>
+            &lt;RadioButton&gt;Choice One&lt;/RadioButton&gt;<br/>
+            &lt;RadioButton isDisabled&gt;Choice Two&lt;/RadioButton&gt;<br/>
+            &lt;RadioButton&gt;Choice Three&lt;/RadioButton&gt;<br/>
+          &lt;/RadioField&gt;<br/>
+        </code>
+      </pre>
+      <RadioField>
+        <RadioButton>Choice One</RadioButton>
+        <RadioButton isDisabled>Choice Two</RadioButton>
         <RadioButton>Choice Three</RadioButton>
       </RadioField>
     </>
