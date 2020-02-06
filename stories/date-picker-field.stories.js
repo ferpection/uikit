3eb736303f9baef8a3093067868e02f2547bdc5c
Mindsers <dev@nathanaelcherrier.com>
2020-02-06 17:34:46 +0100

    Add documentation

--- stories/date-picker-field.stories.js
+++ stories/date-picker-field.stories.js
@@ -4,11 +4,17 @@ import { storiesOf } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs } from '@storybook/addon-knobs'
 
-import { DatePickerField } from '../src/components/index.ts'
+import { DatePickerField, TextField } from '../src/components/index.ts'
 
 storiesOf('Components|Forms/DatePickerField', module)
   .addDecorator(withA11y)
   .addDecorator(withKnobs)
   .add('normal state', () => (
-    <DatePickerField />
+    <>
+      <h1>DatePickerField</h1>
+      <p>The component displays a regular {TextField.name} component and when the user click on it a datePicker is displayed on hover.</p>
+      <p>Example:</p>
+      <DatePickerField />
+      <p>The {DatePickerField.name} acts like a regular {TextField.name} plus some specificities.</p>
+    </>
   ))
