9454b872cdcf4f85e146cc05c27c94b472aa8149
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-04-06 21:29:05 +0200

    Update Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
+  ],
+};
