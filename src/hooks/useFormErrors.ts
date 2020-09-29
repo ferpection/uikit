8176a2832265ebbb04166669b0aad22e54798c08
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-09-29 11:52:58 +0200

    :art: Run eslint

--- src/hooks/useFormErrors.ts
+++ src/hooks/useFormErrors.ts
@@ -22,9 +22,9 @@ const validatorsReducer = (errors: { [key: string]: Validator }, error: { name: 
 })
 
 function deepEqual(x: any, y: any): boolean {
-  const ok = Object.keys,
-    tx = typeof x,
-    ty = typeof y
+  const ok = Object.keys
+  const tx = typeof x
+  const ty = typeof y
   return x && y && tx === 'object' && tx === ty
     ? ok(x).length === ok(y).length && ok(x).every(key => deepEqual(x[key], y[key]))
     : x === y
