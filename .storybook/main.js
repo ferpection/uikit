4b3b7e3307caa5be205ee4de0d53035d3bd6c7f0
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-11-10 15:13:10 +0100

    Fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,11 @@ module.exports = {
   stories: ['../**/*.stories.@(js|tsx|mdx)'],
   addons: [
     '@storybook/addon-actions',
+    '@storybook/addon-docs',
     '@storybook/addon-controls',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-viewport',
-    '@storybook/addon-docs',
   ],
 }
