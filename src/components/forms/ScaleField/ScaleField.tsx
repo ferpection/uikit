88a3f05b4b408f508a0c85e6e2725249908ff949
Nathanael CHERRIER <nathanael@cherrier.co>
2021-09-13 21:29:11 +0200

    Let users and devs update the value

--- src/components/forms/ScaleField/ScaleField.tsx
+++ src/components/forms/ScaleField/ScaleField.tsx
@@ -1,19 +1,26 @@
+import { useState, useEffect } from 'react'
+
 import { FormProps } from '../form-props'
 import { baseStyles } from './styles'
 
 interface ScaleFieldProps extends FormProps {
   value?: number
   range?: [number, number]
+  onValueChange?: (value: number) => void
 }
 
 export function ScaleField(props: ScaleFieldProps) {
-  const { value = 5, range: [minValue = 0, maxValue = 10] = [] } = props
-
+  const { value: initialValue = 5, range: [minValue = 0, maxValue = 10] = [], onValueChange = () => {} } = props
   const choices = Array.from({ length: (maxValue - minValue) + 1 }, (_, i) => minValue + i)
+
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => setValue(initialValue), initialValue)
+  useEffect(() => onValueChange(value), value)
 
   return (
     <div css={[baseStyles]}>
-      {choices.map(choice => <div>{choice}.</div>)}
+      {choices.map(choice => <div onClick={() => setValue(choice)}>{choice}.</div>)}
     </div>
   )
 }
