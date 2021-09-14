6c535d3c3a97644d628a1cca31c0c833f9555017
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-09-14 21:54:38 +0200

    Use theme

--- src/components/forms/ScaleField/ScaleField.tsx
+++ src/components/forms/ScaleField/ScaleField.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 
+import { useTheme } from '../../../hooks/useTheme'
 import { defaultFormProps, FormProps } from '../form-props'
+
 import { baseStyles, itemStyles } from './styles'
 
 interface ScaleFieldProps extends FormProps {
@@ -15,13 +17,14 @@ export function ScaleField(props: ScaleFieldProps) {
   const choices = Array.from({ length: (maxValue - minValue) + 1 }, (_, i) => minValue + i)
 
   const [value, setValue] = useState(initialValue)
+  const theme = useTheme()
 
   useEffect(() => setValue(initialValue), [initialValue])
   useEffect(() => onValueChange(value), [value])
 
   return (
     <div css={[baseStyles]}>
-      {choices.map(choice => <div css={[itemStyles({ selected: choice === value, quantity: choices.length })]} onClick={() => setValue(choice)}>{choice}</div>)}
+      {choices.map(choice => <button key={choice} css={[itemStyles(theme, { selected: choice === value, quantity: choices.length })]} onClick={() => setValue(choice)}>{choice}</button>)}
     </div>
   )
 }
