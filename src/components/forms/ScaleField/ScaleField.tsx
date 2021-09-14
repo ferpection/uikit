f468f7a7d4765ddd5d286b95b841419ea3cb3b61
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-09-14 14:59:24 +0200

    Base style for the ScaleField component

--- src/components/forms/ScaleField/ScaleField.tsx
+++ src/components/forms/ScaleField/ScaleField.tsx
@@ -1,26 +1,34 @@
 import { useState, useEffect } from 'react'
 
-import { FormProps } from '../form-props'
-import { baseStyles } from './styles'
+import { defaultFormProps, FormProps } from '../form-props'
+import { baseStyles, itemStyles } from './styles'
 
 interface ScaleFieldProps extends FormProps {
   value?: number
   range?: [number, number]
+  placeholder?: never
   onValueChange?: (value: number) => void
 }
 
 export function ScaleField(props: ScaleFieldProps) {
-  const { value: initialValue = 5, range: [minValue = 0, maxValue = 10] = [], onValueChange = () => {} } = props
+  const { value: initialValue, range: [minValue = 0, maxValue = 10] = [], onValueChange = () => {} } = props
   const choices = Array.from({ length: (maxValue - minValue) + 1 }, (_, i) => minValue + i)
 
   const [value, setValue] = useState(initialValue)
 
-  useEffect(() => setValue(initialValue), initialValue)
-  useEffect(() => onValueChange(value), value)
+  useEffect(() => setValue(initialValue), [initialValue])
+  useEffect(() => onValueChange(value), [value])
 
   return (
     <div css={[baseStyles]}>
-      {choices.map(choice => <div onClick={() => setValue(choice)}>{choice}.</div>)}
+      {choices.map(choice => <div css={[itemStyles({ selected: choice === value, quantity: choices.length })]} onClick={() => setValue(choice)}>{choice}</div>)}
     </div>
   )
+}
+
+ScaleField.defaultProps = {
+  ...defaultFormProps,
+  range: [1, 10],
+  value: undefined,
+  placeholder: undefined,
 }
--- src/components/forms/ScaleField/styles.ts
+++ src/components/forms/ScaleField/styles.ts
@@ -1,5 +1,47 @@
 import { css } from '@emotion/react'
 
+import { C100_COLOR, C200_COLOR, N300_COLOR, N75_COLOR } from '../../..'
+
 export const baseStyles = css`
   display: flex;
+  margin: 5px;
+`
+
+export const itemStyles = ({ selected = false, quantity = 10 }) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: calc(100% / ${quantity});
+  min-width: min-content;
+  padding: 5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${selected ? C200_COLOR.toRGB() : N75_COLOR.toRGB()};
+  color: ${selected ? C200_COLOR.toRGB() : N300_COLOR.toRGB()};
+  cursor: pointer;
+
+  &:first-of-type {
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;
+    border-right-width: 0;
+  }
+
+  & + & {
+    border-right-width: 0;
+  }
+
+  &:last-of-type {
+    border-top-right-radius: 3px;
+    border-bottom-right-radius: 3px;
+    border-right-width: 1px;
+  }
+
+  &:hover {
+    border-color: ${C100_COLOR.toRGB()};
+    border-right-width: 1px;
+  }
+
+  &:hover + & {
+    border-left-width: 0;
+  }
 `
